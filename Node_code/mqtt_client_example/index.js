b892b81a9bdeed78a310bf242c248e3eb186321b
bozman454 <bosmanwillem94@gmail.com>
2020-11-03 09:42:07 -0500

    fixed

--- Node_code/mqtt_client_example/index.js
+++ Node_code/mqtt_client_example/index.js
@@ -6,11 +6,21 @@ require('dotenv').config();
 
 const url = process.env.BROKER_ADDR;
 const options = {
-    clientId: process.env.BROKER_ADDR
+    clientId: process.env.CLIENT_ID
 }
 
+console.log(`trying to connect to ${url}`)
 const client = mqtt.connect(url,options)
 
 client.on("connect", () => {
-    console.log('on it')
+    console.log('connected')
+    client.publish("testtopic", "test message",{
+        retain:true,
+        qos:1})
 })
+
+
+
+client.on("error", (error) => { console.log("Can't connect"+error)})
+
+
